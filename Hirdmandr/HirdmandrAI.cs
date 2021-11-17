b7a43aa99e47e8ad0514fc43f5eba2d19a620d04
OldManKatan <78036601+OldManKatan@users.noreply.github.com>
2021-11-17 09:08:52 -0600

    Create HirdmandrAI.cs

---
+++ Hirdmandr/HirdmandrAI.cs
@@ -0,0 +1,27 @@
+// Hirdmandr
+// a Valheim mod skeleton using Jötunn
+// 
+// File:    Hirdmandr.cs
+// Project: Hirdmandr
+
+using BepInEx;
+using Jotunn.Configs;
+using Jotunn.Entities;
+using Jotunn.GUI;
+using Jotunn.Managers;
+using Jotunn.Utils;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Hirdmandr
+{
+    [Serializable]
+    public class HirdmandrAI : MonoBehaviour
+    {
+    
+    }
+}
